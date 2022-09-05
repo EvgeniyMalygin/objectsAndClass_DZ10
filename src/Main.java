5804d8e5c257832a7bc4731ca37bc43a173a41e6
Evgeniy Malygin <black2001@inbox.ru>
2022-09-05 12:19:16 +0300

    Реализованы методы toString, equals и hashCode в классах Author и Book

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,13 @@
 public class Main {
     public static void main(String[] args) {
+        Author authorFistBook = new Author("Александр", "Пушкин");
+        Author authorSecondBook = new Author("Александр", "Грибоедов");
+        Book fistBook = new Book("Сборник стихов", "1979", authorFistBook);
+        Book secondBook = new Book("Горе от ума", "1823", authorSecondBook);
+        System.out.println("Первая книга " + fistBook.toString());
+        fistBook.setYearOfPublication("1980");
+        System.out.println("Первая книга " + fistBook.toString());
+        System.out.println("Вторая книга " + secondBook.toString());
 
-        System.out.println("Hello world!");
     }
 }
